12ce1cb9845f1681477669ebdad65767e41dbb45
zs <zhengsheng930118@163.com>
2025-04-14 22:25:48 +0800

    更新到19题

---
+++ 爬虫网/题目19.py
@@ -0,0 +1,63 @@
+import json
+import time
+
+import execjs
+import requests
+
+page = 1
+total = 0
+cookies = {
+    'Hm_lvt_b5d072258d61ab3cd6a9d485aac7f183': '1742952358',
+    'HMACCOUNT': 'F8D55D100B06DC65',
+    'sessionid': 'vmaeexrf3cnbsy6o76brm9pkgy302z33',
+    's': '51b351b351b351b370b0d030d030507090d0f030f0',
+    'v': 'QTJ4b3BBdk1KN3lsLVRQN3UtNXhSbEhrTzBHYkpSTDhFc3NrazhhdGVRMjY5Z0pfN2pYZ1gyTFo5Q0FWMTc0NDM1ODc3MTkzOA==',
+    '_nano_fp': 'XpmYn0EoX5PxX5dxno_SEWXzUnt~HLUABpTu5fic',
+    'Hm_lpvt_b5d072258d61ab3cd6a9d485aac7f183': '1744638330',
+}
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'priority': 'u=1, i',
+    'referer': 'https://stu.tulingpyton.cn/problem-detail/19/',
+    'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+    # 'cookie': 'Hm_lvt_b5d072258d61ab3cd6a9d485aac7f183=1742952358; HMACCOUNT=F8D55D100B06DC65; sessionid=vmaeexrf3cnbsy6o76brm9pkgy302z33; s=51b351b351b351b370b0d030d030507090d0f030f0; v=QTJ4b3BBdk1KN3lsLVRQN3UtNXhSbEhrTzBHYkpSTDhFc3NrazhhdGVRMjY5Z0pfN2pYZ1gyTFo5Q0FWMTc0NDM1ODc3MTkzOA==; _nano_fp=XpmYn0EoX5PxX5dxno_SEWXzUnt~HLUABpTu5fic; Hm_lpvt_b5d072258d61ab3cd6a9d485aac7f183=1744638330',
+}
+
+def des(r, k):
+    with open('./js/19.js', 'r') as f:
+        d = execjs.compile(f.read()).call('decrypt', r, k)
+        return d
+
+while True:
+    params = {
+        'page': page,
+    }
+
+    time.sleep(1)
+    response = requests.get('https://stu.tulingpyton.cn/api/problem-detail/19/data/', cookies=cookies,
+                            headers=headers, params=params)
+
+    if response.status_code != 200:
+        break
+
+    data = response.json()
+
+    d = des(data['r'], data['k'])
+    j = json.loads(d)
+
+    total += sum(j['current_array'])
+    page += 1
+
+    if not j['pagination']['has_next']:
+        break
+
+if __name__ == '__main__':
+    print(total)
